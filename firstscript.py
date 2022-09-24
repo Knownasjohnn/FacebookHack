99e948e380d6642c65a6022a8c0f67fab8cf9694
Knownasjohnn <109334868+Knownasjohnn@users.noreply.github.com>
2022-09-24 17:25:56 +0800

    Add files via upload

---
+++ firstscript.py
@@ -0,0 +1,30 @@
+print("FACEBOOOK BRUTEFORCE")
+name = input("Target Link:")
+print(name)
+
+
+print("loading")
+
+
+print("processing")
+print("10%")
+print("20%")
+print("30%")
+print("40%")
+print("50%")
+print("60%")
+print("70%")
+print("80%")
+print("90%")
+print("100%")
+print("succesfully hacked")
+print("password:fuckyouu_skid")
+
+import datetime
+
+now = datetime.datetime.now()
+print(now)
+
+
+
+
